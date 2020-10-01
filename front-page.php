8230251864089d1d7ba400ae15bc809bd0ddedab
Jacob <jacob.r.goldberg@gmail.com>
2020-09-30 17:24:30 -0700

    styling tweaks

--- front-page.php
+++ front-page.php
@@ -4,9 +4,9 @@
 <!-- Full Page Intro -->
 <div class="view" style="background-color:white;overflow:visible;">
     <!-- Mask & flexbox options-->
-    <div class="mask d-flex justify-content-center " style="background-color:rgba(0,0,0,0);overflow:visible;">
+    <div class="mask d-flex justify-content-center " style="background-color:rgba(0,0,0,0);overflow:visible;position:relative;">
         <!-- Content -->
-        <div style="min-height:100vh; max-height:100% class=" container">
+        <div class="container">
             <!--Grid row-->
             <div class="row justify-content-start">
                 <!--Grid column-->
@@ -50,13 +50,15 @@
                 </div>
             </div>
             <!--Grid row-->
+            <!-- For smaller screens -->
+            <div class="row">
+                <div class="col">
+                    <img class=" img d-lg-none" style="width:85vw" src="<?php echo get_template_directory_uri(); ?>/assets/images/sec1_grydcity.png">
+                </div>
+            </div>
         </div>
         <!-- For larger screens -->
-        <div class="float-right position-absolute d-none d-lg-block" style="top:37vh;left:36vw;z-index:2"><img style="width:90vh ;max-width:1035px; min-width: 530px" src="<?php echo get_template_directory_uri(); ?>/assets/images/sec1_grydcity.png"></div>
-        <!-- For smaller screens -->
-        <!--         <div class="container">
-            <img class=" img d-lg-none" style="width:90vw" src="<?php echo get_template_directory_uri(); ?>/assets/images/sec1_grydcity.png">
-        </div> -->
+        <div class="float-right position-absolute d-none d-lg-block" style="top:25vh;left:36vw;z-index:2"><img style="width:50vw ;max-width:1035px; min-width: 530px" src="<?php echo get_template_directory_uri(); ?>/assets/images/sec1_grydcity.png"></div>
     </div>
     <!-- Content -->
 </div>
@@ -64,16 +66,19 @@
 </div>
 <!-- Full Page Intro -->
 </header>
-<div class="view" style="background-color:var(--light-gray);height:100%">
+<div class="view d-flex justify-content-center align-items-center" style="background-color:var(--light-gray);height:110%;min-height:45vw">
     <!-- Content -->
-    <div class="container d-flex justify-content-center align-items-center my-5" style="min-height:100vh; max-height:100%">
+    <div class="container d-flex justify-content-center align-items-center my-5" style=" min-height:100%; max-width: 75%">
         <div class="row align-items-center justify-content-center">
-            <!-- Image col -->
-            <div class="col-md mx-5 " style="">
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/phone-crop.png" class="img-fluid pt-4" style="max-width:calc(1036px / 2); max-height:calc(1460px / 2); height:80vh">
+            <!-- Image col for large screens-->
+            <div class="col-md d-none d-md-block">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/phone-crop.png" class="img-fluid pt-4" style="max-width:40vw; max-height:80vh">
             </div>
-            <!-- Image col -->
-            <!-- Text col -->
+            <!-- Image col for small screens-->
+            <div class="col-md mx-5 d-md-none">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/phone-crop.png" class="img-fluid pt-4" style="max-width:calc(1036px / 3); max-height:calc(1460px / 3);">
+            </div>
+            <!-- Text col  for large screens-->
             <div style="min-width:40vw " class="col-md-6 d-none d-md-block">
                 <h1 class="charcoal mt-3 h1-connecting">Connecting <span class="primary-green">drivers </span>to<br>available <span class="primary-green"> parking</span> spots.</h1>
                 <div style="margin: 35px 0px;"></div> <!-- spacer -->
@@ -82,7 +87,6 @@
                 <button class="btn main-btn px-5" type="submit"> Learn More </button>
                 <div class="pb-2"></div> <!-- spacer -->
             </div>
-            <!-- Text col -->
             <!-- Text col  for small screens-->
             <div style="max-width:100vw" class="col-md-6 text-center d-md-none">
                 <h1 class="charcoal mt-3" style="font-size:1.9rem">Connecting <span class="primary-green">drivers</span> to<br>available <span class="primary-green"> parking</span> spots.</h1>
@@ -92,13 +96,12 @@
                 <button class="btn main-btn px-5" type="submit"> Learn More </button>
                 <div class="pb-2"></div> <!-- spacer -->
             </div>
-            <!-- Text col -->
         </div>
         <!-- Content -->
     </div>
     <!-- Mask & flexbox options-->
 </div>
-<div class="view" style="background-color:white;height:100%">
+<div class="view" style="background-color:white;height:110%">
     <!-- Content -->
     <div class="container" style="height:100% max-height:1080px">
         <!-- spacer -->
@@ -140,7 +143,7 @@
         <div class="row d-none d-md-flex d-lg-none">
             <div class="col-md-4 mb-4 px-3">
                 <h3>01</h3>
-                <div style="min-height:310px" class="card special-card-1 text-center">
+                <div style="min-height:335px" class="card special-card-1 text-center">
                     <h3 class="primary-green pt-2 bh1">Plan</h3>
                     <p class="bbc">Enter an address, business
                         name or landmark to secure the  perfect parking spot for today  or for and upcoming trip.</p>
@@ -148,7 +151,7 @@
             </div>
             <div class="col-md-4 mb-4 px-3">
                 <h3>02</h3>
-                <div style="min-height:310px" class="card special-card-1 text-center">
+                <div style="min-height:335px" class="card special-card-1 text-center">
                     <h3 class="primary-green pt-2 bh1">Pay</h3>
                     <div class="triangle d-none d-md-block"></div>
                     <p class="bbc">Payment is quick, easy and secure through the grydpark mobile app.</p>
@@ -156,7 +159,7 @@
             </div>
             <div class="col-md-4 mb-4 px-3">
                 <h3>03</h3>
-                <div style="min-height:310px" class="card special-card-1 text-center">
+                <div style="min-height:335px" class="card special-card-1 text-center">
                     <h3 class="primary-green pt-3 bh1">Park</h3>
                     <div class="triangle d-none d-md-block"></div>
                     <p class="bbc">Skip the hassle of parking  and arrive at your reserved spot using the instructions provided.</p>
@@ -248,13 +251,12 @@
                     <h1 class="hqh"> HAVE QUESTIONS?</h1>
                     <button onclick=" help_page()" class="btn main-btn px-5">Grydpark Support Site</button>
                     <script>
-                    function help_page()
-                    {
+                    function help_page() {
                         window.open(
-                          'https://help.parkwithgryd.com/',
-                          '_blank' // <- open in new window.
+                            'https://help.parkwithgryd.com/',
+                            '_blank' // <- open in new window.
                         );
-                    } 
+                    }
                     </script>
                 </div>
             </div>
